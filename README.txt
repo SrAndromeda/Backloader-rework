08c0d6027450e531f54264315b28d3f60fb53724
Mathew <mathew_pi@icloud.com>
2022-01-23 01:14:45 +0300

    Added the ability to download videos in quality higher than 720p. Reflected changes in README.txt .

--- README.txt
+++ README.txt
@@ -1,15 +1,20 @@
+Important notice: as of version 11.0.2 , pytube has a critical bug. 
+Here is how to fix it: https://github.com/pytube/pytube/pull/1203/files
+(All you need to do is type three characters into pytube/cipher.py)
+
 Initial setup
  
     1. Instal dependencies. Run the following commands in your terminal: 
         pip3 install pytube
+        pip3 install moviepy
 
         Never paste random commands from the internet unless you trust the source! You trust me, right? 
 
-    2. (Optional) If you want your videos to be downloaded to a specific folder, paste the path to your folder into #.download(output_path = "YOUR_PATH_HERE") on line 57. Remove the "#" and ".download()" to the left. 
+    2. (Optional) If you want your videos to be downloaded to a specific folder, paste the full path to your folder into pathToTargetFolder = "" on line 24.
         Example for Windows:
-        filters.get_highest_resolution().download("C:\Downloads\YouTube\PythonForDummies\") 
+        pathToTargetFolder = "C:\Users\Name\Downloads\YouTube\PythonForDummies\"
         Example for good operating systems:
-        filters.get_highest_resolution().download("/home/pixselious/Downloads/YouTube/RicingArtixSeason5") 
+        pathToTargetFolder = "/home/pixselious/Downloads/YouTube/RicingArtixSeason5"
 
     3. Follow command line prompts. Prepare a link to your YouTube playlist. Use YouTube's share button to avoid any confusion. 
         Example:
@@ -19,10 +24,10 @@ Initial setup
 
 Random notes:
 
+    YouTube stores files with both video and sound only in resolutions up to 720p. To get a higher resolution video, this script downloads video and audio separatley and then merges them on your computer. This requires either a lot of time or a powerful computer.
+
     There is currently no way of "politely" stopping the script, so you will have to just end that process.
 
-    Do not set the time interval between checks to less that a couple minutes, or you will ddos your pc and google's servers.
-
-    This script will download videos in 720p. Make sure that you have plenty of free space on your drive.
-
     If you see "Local video no longer exists in the playlist" don't worry, that means that last time the program checked, there was a certain video in the YouTube playlist and now there isn't. The script will not stop working and won't download that video again if it reappears in the playlist.
+
+    This script does not support HDR videos, they will be compressed into SDR. This is a limitation of the moviepy library. I don't need to download HDR videos, so they won't be supported anytime soon. But feel free to do it yourself, after all this project is open-source!
--- backloader.py
+++ backloader.py
@@ -21,12 +21,16 @@
 
 showLicenceNotice = True
 
+pathToTargetFolder = ""
 
 
 from time import sleep
 
 from pytube import YouTube, Playlist
+from moviepy.editor import *
 
+import os
+import shutil
 import ssl
 
 ssl._create_default_https_context = ssl._create_stdlib_context
@@ -38,7 +42,7 @@ def getUrls(playlistUrl):
 
     playlist = Playlist(playlistUrl)
     
-    urls = playlist.video_urls
+    urls = list(playlist.video_urls)
     
     return(urls)
 
@@ -53,13 +57,63 @@ def download(url):
     try:
         yt_obj = YouTube(url)
     
-        filters = yt_obj.streams.filter(progressive=True, file_extension='mp4')
-        filters.get_highest_resolution().download() #.download(output_path = "YOUR_PATH_HERE")
-        print('Downloaded ' + url)
+        filters = yt_obj.streams.filter(progressive=True, file_extension = "mp4")
+        
+        if len(pathToTargetFolder) == 0:
+            filters.get_highest_resolution().download()
+        else:
+            filters.get_highest_resolution().download(output_path = pathToTargetFolder)
+        
+        print('Downloaded')
+        print("")
         
     except Exception as e:
         print(e)
     
+def downloadHiRes(url):
+    
+    print('Started ' + url)
+    
+    try:
+        youtube = YouTube(url)
+    
+        # Downloading highest resolution video and audio
+    
+        video = youtube.streams.filter(only_video = True, file_extension = "mp4")
+        video.desc()
+        video.first().download(filename = "temp_video_" + url.split("v=")[-1] + ".mp4")
+        
+        audio = youtube.streams
+        audio.get_audio_only().download(filename = "temp_audio_" + url.split("v=")[-1] + ".mp4")
+        
+        # Merging video and audio together
+        
+        print('Merging video and audio, this might take a while')
+        
+        videoClip = VideoFileClip("temp_video_" + url.split("v=")[-1] + ".mp4")
+        audioClip = AudioFileClip("temp_audio_" + url.split("v=")[-1] + ".mp4")
+        
+        completeClip = videoClip.set_audio(audioClip)
+        completeClip.write_videofile(filename = "final_" + url.split("v=")[-1] + ".mp4", audio_codec = "aac")
+        
+        # Removing temporary video and audio files
+        
+        os.remove("temp_video_" + url.split("v=")[-1] + ".mp4")
+        os.remove("temp_audio_" + url.split("v=")[-1] + ".mp4")
+        
+        # Renaming final file here instead of during creation to not confuse the terminal with emojis or anything like that.
+        
+        newName = youtube.title + ".mp4"
+        os.rename("final_" + url.split("v=")[-1] + ".mp4", newName)
+        
+        if len(pathToTargetFolder) != 0:
+            shutil.move(newName, pathToTargetFolder)
+        
+        print('Merged')
+        print("")
+        
+    except Exception as e:
+        print(e)
     
     
 #   LOGGING DOWNLOADED VIDEOS
@@ -75,7 +129,7 @@ except:
 def appendFile(newUrls):
     with open("downloadedUrls" + '.txt', 'a') as file:
         for url in newUrls:
-            file.write(url + " ") # Im sick and tired of trying to write each url on a new line I cant make it read properly
+            file.write(url + " ")
 
 # Used to fetch urls of previously downloaded videos to compare with urls from YT playlist
 def readFile():
@@ -109,9 +163,12 @@ def checkPlaylist(playlistUrl):
     
     return(newVideos)
 
-def downloadVideos(urls):
+def downloadVideos(urls, resolution):
     for url in urls:
-        download(url)
+        if resolution == True:
+            downloadHiRes(url)
+        else:
+            download(url)
         
         
         
@@ -124,25 +181,35 @@ if showLicenceNotice == True:
     This is free software, and you are welcome to redistribute it
     under certain conditions; Read LICENCE.txt for details.
     
-    Thank you to the contributors of pytube
+    Thank you to the contributors of pytube and moviepy
     """)
 
-playlistUrl = input("Enter playlist URL:")
-ignoreCurrent = input("Do you want to download videos thet are already in this playlist? (Y/N)")
-interval = int(input("Enter time interval between checks in seconds (1 hour = 3600 seconds, 1 day = 86400 seconds):"))
-count = input("Enter total amount of checks you want to run:")
+playlistUrl = input("Enter playlist URL: ")
+ignoreCurrent = input("Do you want to download videos thet are already in this playlist? (Y/N): ")
+print("Downloading videos in the highest quality requires post-processing on your computer. This might cause your computer to heat up and be noisy.")
+print("Downloading videos in 720p does not require post-processing and will be much faster.")
+resolution = input("Do you want to download videos in the highest quality? (Y/N): ")
+interval = int(input("Enter time interval between checks in seconds (1 hour = 3600 seconds, 1 day = 86400 seconds): "))
+count = int(input("Enter total amount of checks you want to run: "))
+
+print("")
+
+if resolution == ("y" or "Y"):
+    hiRes = True
+else:
+    hiRes = False
 
 if ignoreCurrent == ("n" or "N"):
     currentVideos = checkPlaylist(playlistUrl)
     appendFile(currentVideos)
 
-for i in range(int(count)):
+for i in range(count):
     newVideos = checkPlaylist(playlistUrl)
     
     if len(newVideos) == 0:
         print("No new videos")
     else:
-        downloadVideos(newVideos)
+        downloadVideos(newVideos, hiRes)
         appendFile(newVideos)
     
     print("Waiting " + str(interval) + " seconds")
