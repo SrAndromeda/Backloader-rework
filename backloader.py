18c6cb33fbf5bf9baeb292f1b76f4caff9214feb
Pixselious <bash-egg.0o@icloud.com>
2022-06-05 20:56:11 +0300

    Transitioned from pytube to yt-dlp. A lot of refactoring.

--- backloader.py
+++ backloader.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 """
     
     Backloader will download new videos from a selected YouTube playlist.
@@ -19,33 +20,18 @@
 """
 
 
-showLicenceNotice = True
-
-pathToBaseFolder = "" 
+showLicenceNotice = False
 
 from time import sleep
 
-from pytube import YouTube, Playlist
-from moviepy.editor import *
+from yt_dlp import YoutubeDL
 
 import multiprocessing as mp
-import os
-import shutil
-import ssl
-import urllib.request
 
-ssl._create_default_https_context = ssl._create_stdlib_context
 
 
 #   GETTING URL OF VIDEOS FROM A PLAYLIST
 
-def getUrls(playlistUrl):
-    # Getting URLs of videos from a playliat. Returns a list of strings
-    playlist = Playlist(playlistUrl)
-    
-    urls = list(playlist.video_urls)
-    
-    return(urls)
 
 def URLtoID(url):
     # Converts URLs from pytube to IDs for the database. This only works for regular short links, like the ones we get from pytube. Returns a string.
@@ -62,133 +48,51 @@ def removeBadChars(string):
 
 #   DOWNLOADING VIDEOS
 
-def downloadProgressive(url, pathToTargetFolder):
-    # Download video in the highest progressive (image + sound) resolution. Returns a Video() object. 
-    print('Started ' + url)
+
+def download(url, resolution, pathToTargetFolder, flowId):
+    
+    #Selecting resolution
+    if str(resolution) == '720':
+        format = 'best'
+    elif str(resolution) == 'MAX':
+        format = 'bestaudio+bestvideo/best'
+    elif str(resolution) == 'AUDIO':
+        format = 'bestaudio'
+    else:
+        format = 'bestaudio+bestvideo[height={}]/bestaudio+bestvideo/best'.format(str(resolution)) #Selecting best audio and merging it to video of requested resolution. If NA, fall back to best separate streams, if NA fall back to best progressive stream.
+    
+    
     
     try:
-        youtube = YouTube(url)
-    
-        filters = youtube.streams.filter(progressive=True, file_extension = "mp4")
         
-        filters.get_highest_resolution().download(output_path = pathToTargetFolder + "/" + removeBadChars(youtube.title), filename = removeBadChars(youtube.title) + ".mp4")
+        ydl_opts = {
+            'format': format, 
+            'forcefilename': 'True',
+            'writethumbnail': 'True',
+            'download_archive': 'downloaded {}.txt'.format(flowId),
+            'writeinfojson': 'True',
+            'merge_output_format': 'mp4',
+            'paths': {
+                'home': pathToTargetFolder,
+                'temp': pathToTargetFolder + '/tmp'
+            },
+            'outtmpl': {
+                'default': '%(title)s [%(id)s]/%(title)s [%(id)s].%(ext)s',
+                'thumbnail': '%(title)s [%(id)s]/%(title)s [%(id)s].%(ext)s',
+                'infojson': '%(title)s [%(id)s]/%(title)s [%(id)s].%(ext)s'
+                
+            },
+            'quiet': 'True'
+        }
         
-        # Download the thumbnail
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([ "%s" %url ]) # It wants me to pass a list of urls so i'm creating one right here
         
-        urllib.request.urlretrieve(youtube.thumbnail_url, pathToTargetFolder + "/" + removeBadChars(youtube.title) + "/poster.jpg")
-        
-        print('Downloaded')
-        
-    except Exception as e:
-        print(e)
-        
-def downloadHiRes(url, resolution, pathToTargetFolder):
-    # Download video and audio separately and them merge them. Returns a Video() object.
-    resolution = resolution + "p"
-    
-    print('Started ' + url)
-    
-    try:
-        youtube = YouTube(url)
-    
-        # Getting all available resolutions
-    
-        video = youtube.streams.filter(only_video = True, file_extension = "mp4")
-        
-        # Checking if user requested a specific resolution
-        if resolution != "MAXp":
-            # Checking if the requested resolution is available
-            temp = video.filter(res = resolution)
-            
-            if len(temp) == 0:
-                # If the video is not available in the requested resolution, get the highest possible.
-                print("Requested resolution is not available")
-                video.desc()
-            else:
-                video = temp
-        
-        
-        video.first().download(filename = "temp_video_" + URLtoID(url) + ".mp4")
-        
-        # Downloading the audio
-        
-        audio = youtube.streams
-        audio.get_audio_only().download(filename = "temp_audio_" + URLtoID(url) + ".mp4")
-        
-        # Merging video and audio together
-        
-        print('Merging video and audio, this might take a while')
-        
-        videoClip = VideoFileClip(os.path.normcase("temp_video_" + URLtoID(url) + ".mp4"))
-        audioClip = AudioFileClip(os.path.normcase("temp_audio_" + URLtoID(url) + ".mp4"))
-        
-        completeClip = videoClip.set_audio(audioClip)
-        completeClip.write_videofile(filename = "final_" + url.split("v=")[-1] + ".mp4", audio_codec = "aac")
-        
-        # Removing temporary video and audio files
-        
-        os.remove(os.path.normcase("temp_video_" + URLtoID(url) + ".mp4"))
-        os.remove(os.path.normcase("temp_audio_" + URLtoID(url) + ".mp4"))
-        
-        shutil.move(URLtoID(url) + ".mp4", os.path.normcase("./videos"))
-        
-    
-        # Renaming final file here instead of during creation to not confuse the terminal with emojis or anything like that.
-        
-        #TODO: give savers a complete path instead of them coming up with a new one on their own
-        
-        newName = removeBadChars(youtube.title) + ".mp4"
-        os.rename("final_" + url.split("v=")[-1] + ".mp4", newName)
-        
-        pathToTargetFolder = pathToTargetFolder + "/" + removeBadChars(youtube.title)
-        
-        shutil.move(newName, pathToTargetFolder)
-        urllib.request.urlretrieve(youtube.thumbnail_url, pathToTargetFolder + "/poster.jpg")
-            
-        
-        print('Merged')
-        print("")
         
     except Exception as e:
         print(e)
 
 
-    
-#   LOGGING DOWNLOADED VIDEOS
-
-# Creates a log file on the first run 
-#TODO: put this in a for loop under if __name__ == '__main__':
-try:
-    file = open(pathToBaseFolder + "/downloadedUrls" + '.txt', 'x')
-    file.close
-except:
-    pass
-#TODO: make separate files for different playlists
-# Used to add urls of newly downloaded videos to a log file
-def appendFile(newUrls):
-    with open(pathToBaseFolder + "/downloadedUrls" + '.txt', 'a') as file:
-        for url in newUrls:
-            file.write(url + " ")
-
-# Used to fetch urls of previously downloaded videos to compare with urls from YT playlist
-def readFile():
-    urls = []
-    with open(pathToBaseFolder + "/downloadedUrls" + '.txt', 'r') as file:
-        raw = file.read()
-    urls = raw.split()
-    return(urls)
-
-# Shows new videos in the playlist
-def compare(local, online): 
-    
-    for url in local:
-        try:
-            online.remove(url)
-        except:
-            print("Local video no longer exists in the playlist")
-    
-    difference = online 
-    return difference    
 
 
 #   SAVING FLOW SETTINGS AND OTHER FLOW HELPER FUNCTIONS
@@ -213,9 +117,14 @@ flowProcesses = []
 
 def getPlaylistName(url):
     # Gets the name of a playlist
-    playlist = Playlist(url = url)
-    name = playlist.title
-    return(name)
+        
+    with YoutubeDL({'quiet': 'True',}) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        #playlist_url = info_dict.get("url", None)
+        #playlist_id = info_dict.get("id", None)
+        playlist_title = info_dict.get('title', None)
+
+    return(playlist_title) 
 
 # Creates a dictionary and adds it to the list
 def createFlow(workingDirectory, interval, limit, resolution, playlistUrl):
@@ -239,21 +148,9 @@ def createFlow(workingDirectory, interval, limit, resolution, playlistUrl):
  
     
 
-#   CHECKING FOR NEW VIDEOS AND DOWNLOADING THEM
 
-def checkPlaylist(playlistUrl):
-    local = readFile()
-    online = getUrls(playlistUrl)
-    newVideos = compare(local, online)
-    
-    return(newVideos)
 
-def downloadVideos(urls, resolution, path):
-    for url in urls:
-        if resolution == "720":
-            downloadProgressive(url, path)
-        else:
-            downloadHiRes(url, resolution, path)
+
         
         
 #   UI
@@ -265,7 +162,7 @@ if showLicenceNotice == True:
     This is free software, and you are welcome to redistribute it
     under certain conditions; Read LICENCE.txt for details.
     
-    Thank you to the contributors of pytube and moviepy
+    Thank you to the contributors of yt-dlp
     """)
 
 print("")
@@ -273,43 +170,25 @@ print("")
 
 def flowInstance (id, name, workingDirectory, interval, limit, resolution, playlistUrl):
 
-    print("Launched flow " + name)
-    
-    #currentVideos = checkPlaylist(playlistUrl)
-    #appendFile(currentVideos) 
-
-    if limit == 0:
+    print(name+" Flow started")
         
+    if limit == 0:   
         while True:
-            newVideos = checkPlaylist(playlistUrl)
-            
-            if len(newVideos) == 0:
-                print("No new videos")
-            else:
-                downloadVideos(newVideos, resolution, workingDirectory)
-                appendFile(newVideos)
-            
-            print("Waiting " + str(interval) + " seconds")
+            print(name+" Checking playlist")
+            download(playlistUrl, resolution, workingDirectory, id)    
+            print(name+" Waiting " + str(interval) + " seconds")
             sleep(interval)
-            
     else:
-            
         for i in range(limit):
-            newVideos = checkPlaylist(playlistUrl)
-            
-            if len(newVideos) == 0:
-                print("No new videos")
-            else:
-                downloadVideos(newVideos, resolution, workingDirectory)
-                appendFile(newVideos)
-            
-            print("Waiting " + str(interval) + " seconds")
+            print(name+" Checking playlist")
+            download(playlistUrl, resolution, workingDirectory, id)
+            print(name+" Waiting " + str(interval) + " seconds")
             sleep(interval)
-    
             
             i += 1
-            
-        print("Reached limit of checks")
+        print(name+" Reached limit of checks")
+        
+    print(name+" Flow ended")
         
     
     
@@ -322,8 +201,7 @@ if __name__ == '__main__':
     
     #TODO: Load flowData from memory
     
-    # Debug
-    createFlow(workingDirectory= "", interval= 3, limit= 3, resolution= "720", playlistUrl= "")
+    createFlow(workingDirectory= "Path to your media folder here (a library folder will be created automatically)", interval= 3, limit= 3, resolution= "720", playlistUrl= "Link to your playlist here")
     
     for flow in flowData:
         flowProcesses.append(mp.Process(target = flowInstance, name = "Backloader " + flow["name"], args = (flow["id"], flow["name"], flow["workingDirectory"], flow["interval"], flow["limit"], flow["resolution"], flow["playlistUrl"])))
