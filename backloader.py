3b93d7df7bb75ec7ba60ef5f970e74bd064d0c6d
Pixselious <bash-egg.0o@icloud.com>
2022-06-06 18:51:48 +0300

    Slight tweaks, new comments and trying to make Music library type work.

--- backloader.py
+++ backloader.py
@@ -53,36 +53,44 @@ def download(url, resolution, pathToTargetFolder, flowId):
     
     #Selecting resolution
     if str(resolution) == '720':
-        format = 'best'
+        format = 'best' # Selecting the best progressive (video and audio in one file) stream
     elif str(resolution) == 'MAX':
-        format = 'bestaudio+bestvideo/best'
+        format = 'bestaudio+bestvideo/best' #Selecting best audio and merging it to best video. If NA, fall back to best progressive stream.
     elif str(resolution) == 'AUDIO':
-        format = 'bestaudio'
+        format = 'bestaudio' #Selecting the best audio only stream 
     else:
         format = 'bestaudio+bestvideo[height={}]/bestaudio+bestvideo/best'.format(str(resolution)) #Selecting best audio and merging it to video of requested resolution. If NA, fall back to best separate streams, if NA fall back to best progressive stream.
     
+    #Selecting appropriate output templates for each supported library type
+    #Guide for these unreadable blobs https://github.com/yt-dlp/yt-dlp#output-template
+    if str(resolution) == 'AUDIO':
+        outtmpl = {
+                'default': '%(channel)s /%(title)s [%(id)s].%(ext)s'
+        } # Music library type https://jellyfin.org/docs/general/server/media/music.html
+    else:
+        outtmpl = {
+                'default': '%(title)s [%(id)s]/%(title)s [%(id)s].%(ext)s',
+                'thumbnail': '%(title)s [%(id)s]/poster.%(ext)s',
+                'infojson': '%(title)s [%(id)s]/%(title)s [%(id)s].%(ext)s'
+            } # Movies library type https://jellyfin.org/docs/general/server/media/movies.html
     
     
     try:
         
         ydl_opts = {
-            'format': format, 
-            'forcefilename': 'True',
-            'writethumbnail': 'True',
-            'download_archive': 'downloaded {}.txt'.format(flowId),
-            'writeinfojson': 'True',
-            'merge_output_format': 'mp4',
+            'format': format,       #What to download
+            'writethumbnail': 'True',       #Download the thumbnail
+            'download_archive': 'downloaded {}.txt'.format(flowId),         #Keep a record of  downloaded videos
+            'writeinfojson': 'True',        #Download video metadata (description, author, etc)
+            'merge_output_format': 'mp4',       #Keep videos mp4
             'paths': {
                 'home': pathToTargetFolder,
                 'temp': pathToTargetFolder + '/tmp'
-            },
-            'outtmpl': {
-                'default': '%(title)s [%(id)s]/%(title)s [%(id)s].%(ext)s',
-                'thumbnail': '%(title)s [%(id)s]/%(title)s [%(id)s].%(ext)s',
-                'infojson': '%(title)s [%(id)s]/%(title)s [%(id)s].%(ext)s'
-                
-            },
-            'quiet': 'True'
+            },      #Where to save the videos
+            'outtmpl': outtmpl,         #Where to save the videos and how to name them
+            'quiet': 'True',        #Do not show useless info (warnings are still being shown)
+            'progrss': 'True',       #Show progress bars   
+            'embed_metadata': 'True'        #Embed the thumbnail, subtitles, chapters and other metadata when possible 
         }
         
         with YoutubeDL(ydl_opts) as ydl:
